11be9a9f7a5a7da9246d020666738de9e1ed1a93
Dany.A <arabodanny002@gmail.com>
2025-12-26 20:22:38 -0500

    Add check for existence of mitre.svg file

--- scripts/render_mitre_png.py
+++ scripts/render_mitre_png.py
@@ -1,4 +1,8 @@
+import os
 import cairosvg
+
+if not os.path.exists("assets/mitre.svg"):
+    raise FileNotFoundError("SVG file does not exist. Run build_mitre_svg.py first.")
 
 cairosvg.svg2png(
     url="assets/mitre.svg",
